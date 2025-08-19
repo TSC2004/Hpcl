d6c5b74b222ad68fddefb4c78041342ab1133527
TSC2004 <tejaschougule234@gmail.com>
2025-08-19 23:53:48 +0530

    Add files via upload

---
+++ Assignment 4/Fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define N 20   
+
+int main() {
+    int fib[N];
+    fib[0] = 0;
+    fib[1] = 1;
+
+    double start, end;
+    start = omp_get_wtime();
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            printf("Thread %d initialized first two terms\n", omp_get_thread_num());
+        }
+        #pragma omp for schedule(static) nowait
+        for (int i = 2; i < N; i++) {
+            #pragma omp critical
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+            printf("Thread %d computed fib[%d] = %d\n", omp_get_thread_num(), i, fib[i]);
+        }
+
+        #pragma omp barrier
+
+        #pragma omp for schedule(static)
+        for (int i = 0; i < N; i++) {
+            #pragma omp critical
+            {
+                printf("fib[%d] = %d\n", i, fib[i]);
+            }
+        }
+    }
+
+    end = omp_get_wtime();
+    printf("Execution Time: %f seconds\n", end - start);
+
+    return 0;
+}
